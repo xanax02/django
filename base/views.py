e8fa9ffcb7a74e31b57228a20f1d2e302cf0e9ef
abhay <abhaytxanax@gmail.com>
2023-08-08 11:38:00 +0530

    rendering-list-inside-templates

--- base/views.py
+++ base/views.py
@@ -3,9 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Lets learn python!'},
+    {'id': 3, 'name': 'Lets learn python!'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 
 def room(request):
