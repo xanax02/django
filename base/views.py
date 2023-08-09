f83f51727be091c76fcada68d0f24c847d4d06c4
abhay <abhaytxanax@gmail.com>
2023-08-09 11:50:33 +0530

    django-forms

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -13,3 +14,28 @@ def room(request, id):
     room = Room.objects.get(id=id)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if (request.method == 'POST'):
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, id):
+    room = Room.objects.get(id=id)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
