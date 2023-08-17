8d413b57768f02e4fe4f212fb733d4b1df5cdc69
abhay <abhaytxanax@gmail.com>
2023-08-17 20:27:32 +0530

    searchbar

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -7,9 +8,15 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(discription__icontains=q)
+    )
+    room_count = rooms.count()
+
     topics = Topic.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms, 'topics': topics})
+    return render(request, 'base/home.html', {'rooms': rooms, 'topics': topics, 'room_count': room_count})
 
 
 def room(request, id):
