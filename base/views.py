a20574cabc4bbb6072b537e159eec6dcf59fdf4e
abhay <abhaytxanax@gmail.com>
2023-08-21 16:07:17 +0530

    authenticating_user

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room, Topic
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RoomForm
 
@@ -14,9 +15,15 @@ def loginPage(request):
         password = request.POST.get('password')
 
         try:
-            User.objects.all(username=username)
+            User.objects.get(username=username)
         except:
             messages.error(request, 'user doesnot exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request=request, user=user)
+            return redirect('home')
 
     context = {}
     return render(request, "base/login_register.html", context)
