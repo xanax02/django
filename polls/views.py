05d338b8676b4e5b119ab39690fbfdd20a47bbdc
abhay <abhaytxanax@gmail.com>
2023-09-07 17:38:43 +0530

    getobject_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse , Http404
 # from django.template import loader
 from .models import Question
@@ -13,10 +13,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(id=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question Does not exist")
+    # try:
+        # question = Question.objects.get(id=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question Does not exist")
+    question = get_object_or_404(Question, id = question_id)
     return render(request, 'polls/index.html', {"question": question})
 
 
