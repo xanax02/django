fc85d355ea92d63420c4d9b94a5be9b6aeb64b5a
abhay <abhaytxanax@gmail.com>
2023-09-07 19:12:16 +0530

    namespacing urls

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse , Http404
 # from django.template import loader
+
 from .models import Question
 
 # Create your views here.
@@ -18,7 +19,7 @@ def detail(request, question_id):
     # except Question.DoesNotExist:
     #     raise Http404("Question Does not exist")
     question = get_object_or_404(Question, id = question_id)
-    return render(request, 'polls/index.html', {"question": question})
+    return render(request, 'polls/details.html', {"question": question})
 
 
 def results(request, question_id):
