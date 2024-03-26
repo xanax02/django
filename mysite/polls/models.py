a2ea347a8c581595d1ec8a29d425c9bafade37ca
abhay <abhaytxanax@gmail.com>
2024-03-26 21:17:02 +0530

    recetnly published method

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -8,6 +11,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    def was_published_recently(self):
+        return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
